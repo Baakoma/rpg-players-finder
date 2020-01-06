8632632d00a8843b0b512c06067c8e29c0dba60f
Dawid <12dav21@gmail.com>
2020-01-06 23:21:51 +0100

    email verification

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Auth::routes(['verify' => true]);
+
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
-Route::group(['middleware' => ['auth.jwt']], function () {
+Route::group(['middleware' => ['auth.jwt','verified']], function () {
     Route::post('logout', 'AuthController@logout');
 });
