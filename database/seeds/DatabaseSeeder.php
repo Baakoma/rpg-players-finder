c63fcae26bd4aee0d1f62a98d3073af6e7cef9ff
DG <lol3056@gmail.com>
2020-01-10 22:47:14 +0100

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,6 +7,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CreateUserSeeder::class,
             LanguagesTableSeeder::class,
             SystemsTableSeeder::class,
             ProfilesTableSeeder::class
