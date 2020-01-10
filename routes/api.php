00d58448809ca2675d7cdccf29f33a7e96b47801
DG <lol3056@gmail.com>
2020-01-10 23:09:47 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'AuthController@login');
-Route::post('register', 'AuthController@register');
-
-Route::group(['middleware' => ['auth.jwt']], function () {
-    Route::post('logout', 'AuthController@logout');
-});
-
 Route::get('/profile/{profile}', 'ProfileController@show');
 Route::put('/profile/{profile}', 'ProfileController@update');
