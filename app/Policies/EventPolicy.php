7d5cccfe1585a332d73f48c622f792a5ced9f4f3
DG <lol3056@gmail.com>
2020-05-31 11:21:31 +0200

    Add event authorization

---
+++ app/Policies/EventPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\{Event, User};
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EventPolicy
+{
+    use HandlesAuthorization;
+
+    public function view(User $user, Event $event) : bool
+    {
+        return $event->public_access || $user->is(User::findOrFail($event->owner_id)) || $event->playerExist($user->id);
+    }
+
+    public function modify(User $user, Event $event) : bool
+    {
+        return $user->is(User::findOrFail($event->owner_id));
+    }
+}
